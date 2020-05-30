4f537db1d39ac69133eb9962b1bbdc76bed21b60
Artem_Sdobnikov <artem.sdobnikov25@gmail.com>
2020-05-30 11:08:15 +0300

    create build folder

---
+++ build/precache-manifest.20f61939f683ab88dd9b21682216fe14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b3931160d97865b0b83c51bd1534fc5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "466aced2588f72f6068e",
+    "url": "/static/css/2.63b65d4e.chunk.css"
+  },
+  {
+    "revision": "89853240da64ef227a54",
+    "url": "/static/css/main.84274fca.chunk.css"
+  },
+  {
+    "revision": "466aced2588f72f6068e",
+    "url": "/static/js/2.3a8b65cf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.3a8b65cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89853240da64ef227a54",
+    "url": "/static/js/main.a2c9838c.chunk.js"
+  },
+  {
+    "revision": "1cf968120c2e0622953e",
+    "url": "/static/js/runtime-main.eb0b0016.js"
+  }
+]);
